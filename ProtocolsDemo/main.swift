a8fabc3dcfcd6b239b6566fbdc1f4854f571b538
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-01 11:04:45 +0100

    add CanFly protocol

--- ProtocolsDemo/main.swift
+++ ProtocolsDemo/main.swift
@@ -1,4 +1,6 @@
-
+protocol CanFly{
+    func fly()
+}
 class Bird{
     var isFemale = true
     
@@ -7,20 +9,41 @@ class Bird{
             print("The bird makes a new bird in a shell.")
         }
     }
-
-    func fly(){
-        print("The bird flaps its wings and lifts off into the sky.")
-    }
         
 }
 
-class Eagle: Bird{
+class Eagle: Bird, CanFly{
     func soar(){
         print("The eagle glides in the air using air currents.")
     }
+    func fly(){
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+}
+class Penguin: Bird{
+    func swim(){
+        print("The penguin paddles through the water.")
+    }
+}
+struct FlyMuseum{
+    func flyingDemo(flyingObject: Eagle){
+        flyingObject.fly()
+    }
 }
 
-
+struct Airplane{
+    
+}
 let myEagle =  Eagle()
-myEagle.fly()
-myEagle.layEgg()
+//myEagle.fly()
+//myEagle.layEgg()
+//myEagle.soar()
+//
+//let myPenguin = Penguin()
+//
+//myPenguin.layEgg()
+//myPenguin.swim()
+//myPenguin.fly()
+
+let museum = FlyMuseum()
+museum.flyingDemo(flyingObject: myEagle)
